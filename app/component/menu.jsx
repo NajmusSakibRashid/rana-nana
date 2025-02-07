4b9ea3ff0bf08e8d3a06dfce153f99fe005c0d51
Najmus Sakib Rashid <sakib.buetian@gmail.com>
2025-02-08 03:27:37 +0600

    shortcut

--- app/component/menu.jsx
+++ app/component/menu.jsx
@@ -1,22 +1,61 @@
-const Menu = ({ items }) => {
-  items && console.log(Object.entries(items));
+const MenuItem = ({ label, url, sub }) => {
+  if (!sub) {
+    // If no sub-items, render a simple link
+    return (
+      <a href={url} className="block px-4 py-2 hover:bg-base-200 rounded-lg">
+        {label}
+      </a>
+    );
+  }
+
+  // If has sub-items, render a collapsible menu
   return (
-    <>
-      <div className="menu dropdown-content bg-base-100 rounded-box z-[1] w-52 p-2 shadow right-0">
-        {items &&
-          Object.entries(items).map(([key, value], index) => (
-            <div key={index} className="collapse bg base-200">
-              <input type="checkbox" name="" id="" />
-              <div className="collapse-title">{key}</div>
-              <div className="collapse-content">
-                <div>
-                  <Menu items={value.sub} />
-                </div>
-              </div>
-            </div>
-          ))}
+    <div className="collapse bg-base-200 rounded-lg">
+      <input type="checkbox" />
+      <div className="collapse-title font-medium flex items-center justify-between">
+        {label}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          className="transition-transform group-focus:rotate-180"
+        >
+          <path d="m6 9 6 6 6-6" />
+        </svg>
       </div>
-    </>
+      <div className="collapse-content">
+        <div className="pl-2 mt-2 border-l-2 border-base-300">
+          {Object.entries(sub).map(([subKey, subValue], index) => (
+            <MenuItem
+              key={index}
+              label={subKey}
+              url={subValue.url}
+              sub={subValue.sub}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const Menu = ({ items }) => {
+  if (!items) return null;
+
+  return (
+    <div className="menu dropdown-content bg-base-100 rounded-box z-[1] p-2 shadow right-0 w-screen md:left-0 md:w-52">
+      {Object.entries(items).map(([key, value], index) => (
+        <div key={index} className="mb-1">
+          <MenuItem label={key} url={value.url} sub={value.sub} />
+        </div>
+      ))}
+    </div>
   );
 };
 
--- app/component/navbar.jsx
+++ app/component/navbar.jsx
@@ -1,6 +1,6 @@
 import NavbarElements from "../assets/navbar-elements";
 import Menu from "./menu";
-import logo from "../assets/logo.jpg";
+import logo from "../assets/bandhan-modified.png";
 import Image from "next/image";
 
 const navbar = () => {
@@ -8,13 +8,35 @@ const navbar = () => {
     <div>
       <div className="navbar bg-base-100 justify-between">
         <Image src={logo} alt="logo" width={100} height={100} />
-        <div className="flex-1 hidden md:flex">
+        <div className="flex-1 hidden md:flex justify-end">
           {/* <a className="btn btn-ghost text-xl">daisyUI</a> */}
           {Object.entries(NavbarElements).map(([key, value], index) => {
             return (
-              <a key={index} className="btn btn-ghost text-xl">
-                {key}
-              </a>
+              // <a key={index} className="btn btn-ghost text-xl" href={value.url}>
+              //   {key}
+              // </a>
+              <details className="dropdown pl-4 pr-4" key={index}>
+                <summary className="btn btn-square btn-ghost w-full p-2 text-xl">
+                  {key}
+                  {value.sub && (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      className="transition-transform group-focus:rotate-180"
+                    >
+                      <path d="m6 9 6 6 6-6" />
+                    </svg>
+                  )}
+                </summary>
+                <Menu items={value.sub} />
+              </details>
             );
           })}
         </div>
@@ -36,23 +58,6 @@ const navbar = () => {
           </summary>
           <Menu items={NavbarElements} />
         </details>
-        {/* <div className="flex-none">
-          <button className="btn btn-square btn-ghost">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              className="inline-block h-5 w-5 stroke-current"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
-              ></path>
-            </svg>
-          </button>
-        </div> */}
       </div>
     </div>
   );
